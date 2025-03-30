0cffa0523476df4aaec1fefa2d185f5212942071
Chris Fulljames <christianfulljames@gmail.com>
2025-03-30 15:32:17 -0400

    Use new package for service

--- service/run.sh
+++ service/run.sh
@@ -3,5 +3,5 @@ PORT=$1
 export DATA_DIR=$2
 export SECRET_KEY=$(< "$DATA_DIR/secret.key")
 . venv/bin/activate
-gunicorn -w 4 -b 127.0.0.1:$PORT 'main:app'
+gunicorn -w 4 -b 127.0.0.1:$PORT 'littlesongplace:app'
 
