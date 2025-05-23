2d2593e2d27ae9f1d7b58974d67551c821f411aa
Chris Fulljames <christianfulljames@gmail.com>
2025-05-22 20:44:19 -0400

    Delete dead push subscriptions

--- src/littlesongplace/sql/schema_update.sql
+++ src/littlesongplace/sql/schema_update.sql
@@ -1,5 +1,6 @@
 -- DROP TABLE IF EXISTS users_push_subscriptions
 CREATE TABLE users_push_subscriptions (
+    subid INTEGER PRIMARY KEY,
     userid INTEGER NOT NULL,
     subscription TEXT NOT NULL,
     FOREIGN KEY(userid) REFERENCES users(userid) ON DELETE CASCADE
