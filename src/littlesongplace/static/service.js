d50e97525b3664570e707db74616c3c3ea85f02e
Chris Fulljames <christianfulljames@gmail.com>
2025-07-12 12:07:21 -0400

    Add automatic push renewal (untested)

--- src/littlesongplace/static/service.js
+++ src/littlesongplace/static/service.js
@@ -6,4 +6,20 @@ self.addEventListener("push", (event) => {
     }
 });
 
+self.addEventListener("pushsubscriptionchanged", (event) => {
+    console.log("Subscription expired");
+    event.waitUntil(
+        self.registration.pushManager.subscribe({ userVisibleOnly: true })
+        .then((subscription) => {
+            console.log("Register new subscription");
+            const subid = window.localStorage.getItem("subid");
+            return fetch(`/push-notifications/update-subscription/${subid}`, {
+                method: "post",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(subscription)
+            });
+        });
+    );
+});
+
 // TODO: handle notificationclick event
