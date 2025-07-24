dba78d1a681743b89806e7c2358cdfd5747cb28e
Chris Fulljames <christianfulljames@gmail.com>
2025-07-24 06:44:36 -0400

    Use threads to handle simultaneous requests

--- service/run.sh
+++ service/run.sh
@@ -3,5 +3,5 @@ PORT=$1
 export DATA_DIR=$2
 export SECRET_KEY=$(< "$DATA_DIR/secret.key")
 . venv/bin/activate
-gunicorn -w 4 -b 127.0.0.1:$PORT 'littlesongplace:app'
+gunicorn --workers 8 --worker-class gthread --threads 16 -b 127.0.0.1:$PORT 'littlesongplace:app'
 
